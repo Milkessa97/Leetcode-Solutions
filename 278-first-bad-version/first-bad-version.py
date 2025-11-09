1996f92f7c865aec545680edc1218e56dc00a07e
Milkessa97 <milkessahabtamukebu@gmail.com>
2025-11-09 09:34:01 +0300

    Time: 40 ms (37.45%) | Memory: 17.6 MB (69.00%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -5,13 +5,13 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         left, right = 1, n
         while left <= right:
-            mid = (left+right)//2
+            mid = (left+right) // 2
             if isBadVersion(mid):
-                if mid==1:
+                if mid == 1:
                     return 1
                 if isBadVersion(mid-1):
-                    right=mid-1
+                    right = mid - 1
                 else:
                     return mid
             else:
-                left=mid+1
+                left = mid + 1
