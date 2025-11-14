7f6dfda96c7fc41c70e8413c1ca07e6035b8b749
Milkessa97 <milkessahabtamukebu@gmail.com>
2025-11-14 08:36:13 +0300

    Time: 3 ms (17.05%) | Memory: 17.8 MB (46.62%) - LeetSync

--- 1137-height-checker/height-checker.py
+++ 1137-height-checker/height-checker.py
@@ -3,9 +3,9 @@ class Solution:
         expected = sorted(heights)
         p1, p2 = 0, 0
         count = 0
-        while p2<len(heights):
-            if heights[p2]!=expected[p1]:
-                count+=1
-            p1+=1
-            p2+=1
+        while p2 < len(heights):
+            if heights[p2] != expected[p1]:
+                count += 1
+            p1 += 1
+            p2 += 1
         return count
